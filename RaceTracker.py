805d67e7500785df3c7f772efc8b6d85f98ed240
Thomas Hayter <tomhayter24@gmail.com>
2023-09-03 22:03:12 +0100

    Tidy code into methods and remove unnecesary secrets

--- RaceTracker.py
+++ RaceTracker.py
@@ -11,9 +11,67 @@ config = yaml.safe_load(open("./config.yml"))
 CLIENT_ID = config["strava_secrets"]["client_id"]
 CLIENT_SECRET = config["strava_secrets"]["client_secret"]
 CODE = config["strava_secrets"]["code"]
-ACCESS_TOKEN = config["strava_secrets"]["access_token"]
-REFRESH_TOKEN = config["strava_secrets"]["refresh_token"]
 
+RACE_START = datetime(2023, 8, 24)
+
+
+def get_milestones():
+    # Get Milestones
+    spreadsheet = openpyxl.load_workbook("./Running Milestones.xlsx")
+    spreadsheet1 = spreadsheet["Sheet1"]
+
+    milestones = {}
+    for row in range(2, spreadsheet1.max_row):
+        milestones[spreadsheet1.cell(row, 1).value] = spreadsheet1.cell(row, 2).value
+    return milestones
+
+
+def get_nearest_milestones(milestones, total_distance):
+    # Get closest and completed milestones
+    closest_below = (None, -1)
+    closest_above = (None, 50000)
+    completed = []
+
+    for milestone, distance in milestones.items():
+        if distance < total_distance:
+            completed.append((milestone, distance))
+            if distance > closest_below[1]:
+                closest_below = (milestone, distance)
+        elif distance > total_distance:
+            if distance < closest_above[1]:
+                closest_above = (milestone, distance)
+
+    return closest_below, closest_above, completed
+
+
+def get_distance_stats(activities):
+    total_distance = 0
+    run_distance = 0
+    cycle_distance = 0
+    hike_distance = 0
+
+    activities_since_start = []
+    print("Your activities:")
+    for activity in activities:
+        dt = datetime(activity.start_date.year,
+                      activity.start_date.month,
+                      activity.start_date.day)
+        if dt > RACE_START:
+            activities_since_start.append(activity)
+            print(activity.name)
+            # print(activity.type)
+            total_distance += unithelper.kilometer(activity.distance).num
+            if activity.type == "Run":
+                run_distance += unithelper.kilometer(activity.distance).num
+            elif activity.type == "Ride":
+                cycle_distance += unithelper.kilometer(activity.distance).num
+            elif activity.type == "Hike":
+                hike_distance += unithelper.kilometer(activity.distance).num
+
+    return total_distance, run_distance, cycle_distance, hike_distance
+
+
+# Strava authentication
 client = Client()
 # url = client.authorization_url(client_id=CLIENT_ID, redirect_uri='http://127.0.0.1:5000/authorization', scope=['read_all','profile:read_all','activity:read_all'])
 # print(url)
@@ -45,62 +103,20 @@ else:
     client.refresh_token = access_token['refresh_token']
     client.token_expires_at = access_token['expires_at']
 
-# Get Milestones
-spreadsheet = openpyxl.load_workbook("./Running Milestones.xlsx")
-spreadsheet1 = spreadsheet["Sheet1"]
-
-milestones = {}
-for row in range(2, spreadsheet1.max_row):
-    milestones[spreadsheet1.cell(row, 1).value] = spreadsheet1.cell(row, 2).value
-
+milestones = get_milestones()
 print(milestones)
 
+# Get distance data
 athlete = client.get_athlete()
 print(f"Welcome user {athlete.firstname} {athlete.lastname}")
-
 activities = client.get_activities(limit=100)
-
-RACE_START = datetime(2023, 8, 24)
-
-total_distance = 0
-run_distance = 0
-cycle_distance = 0
-hike_distance = 0
-
-activities_since_start = []
-print("Your activities:")
-for activity in activities:
-    dt = datetime(activity.start_date.year,
-                  activity.start_date.month,
-                  activity.start_date.day)
-    if dt > RACE_START:
-        activities_since_start.append(activity)
-        print(activity.name)
-        total_distance += unithelper.kilometer(activity.distance).num
-        if activity.type == "Run":
-            run_distance += unithelper.kilometer(activity.distance).num
-        elif activity.type == "Cycle":
-            cycle_distance += unithelper.kilometer(activity.distance).num
-        elif activity.type == "Hike":
-            hike_distance += unithelper.kilometer(activity.distance).num
+total, run, cycle, hike = get_distance_stats(activities)
+print(f"You have travelled: {total}km.")
+print(f"You have run {run}km, cycled {cycle}km, and hiked {hike}km.")
 
 
-print(f"You have travelled: {total_distance}km.")
-print(f"You have run {run_distance}km, cycled {cycle_distance}km, and hiked {hike_distance}km.")
-
-closest_below = (None, -1)
-closest_above = (None, 50000)
-completed = []
-
-for milestone, distance in milestones.items():
-    if distance < total_distance:
-        completed.append((milestone, distance))
-        if distance > closest_below[1]:
-            closest_below = (milestone, distance)
-    elif distance > total_distance:
-        if distance < closest_above[1]:
-            closest_above = (milestone, distance)
-
+closest_below, closest_above, completed = get_nearest_milestones(milestones, total)
 print(closest_below)
 print(closest_above)
 print(completed)
+
