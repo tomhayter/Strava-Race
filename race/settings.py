6d27cb069f3cf6ea3d3c1f2ac3e58ed1bdf84964
Thomas Hayter <tomhayter24@gmail.com>
2023-12-22 13:54:44 +0000

    Bug fix

--- race/settings.py
+++ race/settings.py
@@ -130,7 +130,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
